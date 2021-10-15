8bce90e6f75b392875d455e74a3e83c1ff901b7e
syamgar <75175864+syamgar@users.noreply.github.com>
2021-10-15 10:55:31 +0530

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('', include('visual.urls')),
     path('attendance/', include('attendance.urls')),
     path('stream/', include('stream.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
